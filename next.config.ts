c9a458037b3c2baf7a139be9821492e246ad53c3
Pranav-Rustagi <pranav001100@gmail.com>
2025-12-01 01:17:05 +0530

    Added serving of static pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/:path',
+        destination: '/:path/index.html',
+      },
+      {
+        source: '/:path/',
+        destination: '/:path/index.html',
+      }
+    ];
+  },
 };
 
 export default nextConfig;
